e87e6dec6c79ef55fe170f54e85fc97c0bd6cb8c
5Buttons <tim.diersmann@outlook.de>
2025-11-24 22:48:29 +0100

    Utgarde Pinnacle: Ymiron Bane warning
    Stopattack for dps and dispell for magicdispellers

--- DBM-Party-WotLK/UtgardePinnacle/Ymiron.lua
+++ DBM-Party-WotLK/UtgardePinnacle/Ymiron.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod.statTypes = "normal,heroic,mythic"
 
-mod:SetRevision("20251123220131")
+mod:SetRevision("20251124220131")
 mod:SetCreatureID(26861)
 mod:SetEncounterID(583)
 
@@ -16,20 +16,13 @@ mod:RegisterEventsInCombat(
 	"SPELL_AURA_REMOVED 48294 59301"
 )
 
-local warningBane		= mod:NewSpecialWarningSoon(48294, "Dps", nil, nil, 1, 2)
+local warningBane		= mod:NewSpecialWarningSpell(48294, "Dps", nil, nil, 1, 2)
 local specWarnBaneDispell	= mod:NewSpecialWarningDispel(48294, "MagicDispeller", nil, nil, 1, 2)
 
 local warningScreams	= mod:NewSpellAnnounce(51750, 2)
 
 local timerBane			= mod:NewBuffActiveTimer(5, 48294, nil, nil, nil, 5, nil, DBM_COMMON_L.MAGIC_ICON)
 local timerScreams		= mod:NewBuffActiveTimer(8, 51750, nil, nil, nil, 2)
-
-function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(48294, 59301) then
-		warningBane:Show()
-		warningBane:Play("stopattack")
-	end
-end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	if args.spellId == 51750 then
@@ -41,8 +34,13 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(48294, 59301) then
 		timerBane:Start()
-		specWarnBaneDispell:Show()
-		specWarnBaneDispell:Play("dispelboss")
+		if self.Options.SpecWarn48294dispel and self:CheckDispelFilter("magic") then
+			specWarnBaneDispell:Show()
+			specWarnBaneDispell:Play("dispelboss")
+		else
+			warningBane:Show()
+			warningBane:Play("stopattack")
+		end
 	end
 end
 
