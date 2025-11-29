17b404f3412bbe97a76afba2d7a8bf24b4b1c852
5Buttons <tim.diersmann@outlook.de>
2025-11-29 22:50:13 +0100

    Halls of Stone: Brann event fix
    -set MinCombatTime to 300s to match the fight duration
    -Added a WatchDog function to recognize a wipe

--- DBM-Party-WotLK/HallsOfStone/BrannBronzebeard.lua
+++ DBM-Party-WotLK/HallsOfStone/BrannBronzebeard.lua
@@ -8,7 +8,7 @@ mod:SetMinSyncRevision(20251123220131)
 
 mod:RegisterCombat("yell", L.Pull)
 mod:RegisterKill("yell", L.Kill)
-mod:SetMinCombatTime(300) --first check for combat set to event duration
+mod:SetMinCombatTime(300)
 mod:SetWipeTime(20)
 
 mod:RegisterEventsInCombat(
@@ -16,39 +16,31 @@ mod:RegisterEventsInCombat(
 )
 
 local warningPhase	= mod:NewAnnounce("WarningPhase", 2, "Interface\\Icons\\Spell_Nature_WispSplode")
-
 local timerEvent	= mod:NewTimer(302, "timerEvent", "Interface\\Icons\\Spell_Holy_BorrowedTime", nil, nil, 6)
 
--- Watchdog function: Forces a wipe if a specific Phase Yell doesn't happen in time.
-local function WatchdogWipe()
-	mod:EndCombat(true)
+function mod:WatchdogWipe()
+	DBM:EndCombat(self, true)
 end
 
 function mod:OnCombatStart(delay)
 	timerEvent:Start(-delay)
-	self:Schedule(35, WatchdogWipe)
+	self:Schedule(35, self.WatchdogWipe, self, "BrannWatchdog")
 end
 
 function mod:CHAT_MSG_MONSTER_YELL(msg)
-	-- Phase 1: Kaddrak ("Security breach...")
-	if msg == L.Phase1 then
+	if msg:find(L.Phase1, 1, true) then
 		warningPhase:Show(1)
-		self:Unschedule(WatchdogWipe)
-		-- WATCHDOG 2: P1 lasts ~75s. Allow 90s for P2 to start.
-		self:Schedule(90, WatchdogWipe)
+		self:Unschedule(self.WatchdogWipe, self, "BrannWatchdog")
+		self:Schedule(90, self.WatchdogWipe, self, "BrannWatchdog")
 
-	-- Phase 2: Marnak ("Threat index...")
-	elseif msg == L.Phase2 then
+	elseif msg:find(L.Phase2, 1, true) then
 		warningPhase:Show(2)
-		self:Unschedule(WatchdogWipe)
-		-- WATCHDOG 3: P2 lasts ~102s. Allow 115s for P3 to start.
-		self:Schedule(115, WatchdogWipe)
+		self:Unschedule(self.WatchdogWipe, self, "BrannWatchdog")
+		self:Schedule(115, self.WatchdogWipe, self, "BrannWatchdog")
 
-	-- Phase 3: Abedneum ("Critical threat...")
-	elseif msg == L.Phase3 then
+	elseif msg:find(L.Phase3, 1, true) then
 		warningPhase:Show(3)
-		self:Unschedule(WatchdogWipe)
-		-- WATCHDOG 4: P3 lasts ~103s. Allow 115s for Kill yell.
-		self:Schedule(115, WatchdogWipe)
+		self:Unschedule(self.WatchdogWipe, self, "BrannWatchdog")
+		self:Schedule(115, self.WatchdogWipe, self, "BrannWatchdog")
 	end
 end
