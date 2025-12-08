cdb21a8ce84b1ee9e9b19336bb5c29a16780e027
5Buttons <tim.diersmann@outlook.de>
2025-12-08 19:35:35 +0100

    Naxxramas: Anubrekhan fixes

--- DBM-Naxx/ArachnidQuarter/Anub'Rekhan.lua
+++ DBM-Naxx/ArachnidQuarter/Anub'Rekhan.lua
@@ -1,16 +1,17 @@
 local mod	= DBM:NewMod("Anub'Rekhan", "DBM-Naxx", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20221106133531")
+mod:SetRevision("20251208133531")
 mod:SetCreatureID(15956)
 mod:SetEncounterID(1107)
 
 mod:RegisterCombat("combat_yell", L.Pull1, L.Pull2)
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 28785 54021",
+	"SPELL_CAST_START 28785 54021 28783",
 	"SPELL_CAST_SUCCESS 28783 56090",
-	"SPELL_AURA_REMOVED 28785 54021"
+	"SPELL_AURA_REMOVED 28785 54021",
+	"UNIT_SPELLCAST_START"
 )
 
 local warningLocustSoon		= mod:NewSoonAnnounce(28785, 2)
@@ -18,23 +19,17 @@ local warningLocustFaded	= mod:NewFadesAnnounce(28785, 1)
 local warnImpale			= mod:NewTargetNoFilterAnnounce(28783, 3, nil, false)
 
 local specialWarningLocust	= mod:NewSpecialWarningSpell(28785, nil, nil, nil, 2, 2)
-local yellImpale			= mod:NewYell(28783, nil, false)
+local yellImpale 			= mod:NewYell(28783, nil, true)
 
-local timerLocustIn			= mod:NewCDTimer(80, 28785, nil, nil, nil, 6)
+local timerLocustIn			= mod:NewCDTimer(90, 28785, nil, nil, nil, 6)
 local timerLocustFade		= mod:NewBuffActiveTimer(23, 28785, nil, nil, nil, 6)
-local timerImpale			= mod:NewCDTimer(13, 56090, nil, nil, nil, 3) -- REVIEW! ~7s variance [13.0-19.8]? (25m Lordaeron 2022/10/16) -- 13.7, 13.6, 19.8, 13.0
+local timerImpale			= mod:NewCDTimer(20, 56090, nil, nil, nil, 3)
 
 mod:AddBoolOption("ArachnophobiaTimer", true, "timer", nil, nil, nil, "at1859")--Sad caveat that 10 and 25 man have own achievements and we have to show only 1 in GUI
 
 function mod:OnCombatStart(delay)
-	if self:IsDifficulty("normal25") then
-		timerLocustIn:Start(100 - delay)
-		warningLocustSoon:Schedule(90 - delay)
-	else
-		timerLocustIn:Start(91 - delay)
-		warningLocustSoon:Schedule(76 - delay)
-	end
-	timerImpale:Start(12.7-delay) -- REVIEW! variance? (25m Lordaeron 2022/10/16) - pull:12.7
+	timerLocustIn:Start("v70-120")  -- Same for both difficulties
+	timerImpale:Start(15-delay)
 end
 
 function mod:OnCombatEnd(wipe)
@@ -53,15 +48,21 @@ function mod:SPELL_CAST_START(args)
 		else
 			timerLocustFade:Start(19)
 		end
+	elseif args:IsSpellID(28783, 56090) then  -- Impale
+		timerImpale:Start()
 	end
 end
 
-function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(28783, 56090) then  -- Impale. REVIEW! 28783 needed?
-		timerImpale:Start()
-		warnImpale:Show(args.destName)
-		if args:IsPlayer() then
-			yellImpale:Yell()
+function mod:UNIT_SPELLCAST_START(unitID, spellName)
+	if spellName == GetSpellInfo(28783) or spellName == GetSpellInfo(56090) then  -- Impale
+		if self:GetUnitCreatureId("target") == 15956 and self:AntiSpam(3, 1) then
+		local targetName = UnitName("targettarget")
+			if targetName then
+				warnImpale:Show(targetName)
+				if targetName == UnitName("player") then
+					yellImpale:Yell()
+				end
+			end
 		end
 	end
 end
@@ -71,6 +72,6 @@ function mod:SPELL_AURA_REMOVED(args)
 	and args.auraType == "BUFF" then
 		warningLocustFaded:Show()
 		timerLocustIn:Start()
-		warningLocustSoon:Schedule(62)
+		warningLocustSoon:Schedule(80)
 	end
 end
