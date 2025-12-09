1cf277ab608fdf80370025b08c5f53b54c949f2c
5Buttons <tim.diersmann@outlook.de>
2025-12-09 21:06:13 +0100

    rev bump

--- DBM-Naxx/PlagueQuarter/Loatheb.lua
+++ DBM-Naxx/PlagueQuarter/Loatheb.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Loatheb", "DBM-Naxx", 3)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20251205220131")
+mod:SetRevision("20251209220131")
 mod:SetCreatureID(16011)
 mod:SetEncounterID(1115)
 
