fe398f42d7e7bd2dab2b14a7c372b8c8032d9a03
5Buttons <tim.diersmann@outlook.de>
2025-12-31 00:39:06 +0100

    Update StrandOfTheAncients.lua
    fixed hp values according to db: https://raw.githubusercontent.com/azerothcore/azerothcore-wotlk/3d15a19fb4ef6298b0c866c34c9acba2978f7a3f/data/sql/base/db_world/gameobject_template.sql

--- DBM-PvP/Battlegrounds/StrandOfTheAncients.lua
+++ DBM-PvP/Battlegrounds/StrandOfTheAncients.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 local GetCurrentMapAreaID = GetCurrentMapAreaID
 
-mod:SetRevision("20250305220503")
+mod:SetRevision("20251231220503")
 mod:SetZone(DBM_DISABLE_ZONE_DETECTION)
 
 mod:RemoveOption("HealthFrame")
@@ -14,25 +14,24 @@ mod:RegisterEvents(
 
 do
 	local bgzone = false
-
 	local function Init()
 		local zoneID = GetCurrentMapAreaID()
 		if not bgzone and zoneID == 513 then
 			bgzone = true
 			local generalMod = DBM:GetModByName("PvPGeneral")
 			generalMod:SubscribeAssault(zoneID, 0)
-			generalMod:TrackHealth(190722, "GreenEmerald", 11000)
-			generalMod:TrackHealth(190724, "BlueSapphire", 11000)
-			generalMod:TrackHealth(190723, "PurpleAmethyst", 13000)
-			generalMod:TrackHealth(190726, "RedSun", 13000)
-			generalMod:TrackHealth(190727, "YellowMoon", 14000)
-			generalMod:TrackHealth(192549, "ChamberAncientRelics", 10000)
+			-- Fixed HP values based on AzerothCore gameobject_template database
+			generalMod:TrackHealth(190722, "GreenEmerald", 2000)
+			generalMod:TrackHealth(190724, "BlueSapphire", 2000)
+			generalMod:TrackHealth(190723, "PurpleAmethyst", 4000)
+			generalMod:TrackHealth(190726, "RedSun", 4000)
+			generalMod:TrackHealth(190727, "YellowMoon", 5000)
+			generalMod:TrackHealth(192549, "ChamberAncientRelics", 5000)
 		elseif bgzone and zoneID ~= 513 then
 			bgzone = false
 			DBM:GetModByName("PvPGeneral"):StopTrackHealth()
 		end
 	end
-
 	function mod:ZONE_CHANGED_NEW_AREA()
 		Init()
 	end
