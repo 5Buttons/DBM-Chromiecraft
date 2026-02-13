e779b7cac67040160afef8d2b1dcc6e309ff0044
5Buttons <tim.diersmann@outlook.de>
2026-02-13 22:31:56 +0100

    Gluth: Fix decimate timer
    removed timerDecimate:Update and use UNIT_SPELLCAST_SUCCEEDED instead of unrealiable SPELL_DAMAGE

--- DBM-Naxx/ConstructQuarter/Gluth.lua
+++ DBM-Naxx/ConstructQuarter/Gluth.lua
@@ -1,19 +1,17 @@
 local mod	= DBM:NewMod("Gluth", "DBM-Naxx", 2)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20250914210600")
+mod:SetRevision("20260213210603")
 mod:SetCreatureID(15932)
 mod:SetEncounterID(1108)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED 28371 54427", -- Added 25-man enrage spell
-	"SPELL_AURA_REMOVED 28371 54427", -- Added 25-man enrage spell
-	"SPELL_DAMAGE 28375 54426"
+	"SPELL_AURA_APPLIED 28371 54427",
+	"SPELL_AURA_REMOVED 28371 54427",
+	"UNIT_SPELLCAST_SUCCEEDED"
 )
 
---TODO, is it really necessary to use SPELL_DAMAGE here?
---TODO, verify decimate timer is actually accurate for wrath (it certainly wasn't for naxx 40)
 local warnEnrage		= mod:NewTargetNoFilterAnnounce(28371, 3, nil , "Healer|Tank|RemoveEnrage", 2)
 local warnDecimateSoon	= mod:NewSoonAnnounce(28374, 2)
 local warnDecimateNow	= mod:NewSpellAnnounce(28374, 3)
@@ -24,26 +22,24 @@ local timerEnrage		= mod:NewBuffActiveTimer(8, 28371, nil, nil, nil, 5, nil, DBM
 local timerDecimate		= mod:NewCDTimer(110, 28374, nil, nil, nil, 2)
 local enrageTimer		= mod:NewBerserkTimer(360)
 
-
 local function getDecimateTimer()
-	if mod:IsDifficulty("normal10", "heroic10") then
-		return 110 -- 10-man: 110 seconds
+	if mod:IsDifficulty("normal25", "heroic25") or mod:IsHeroic() then
+		return 90 -- 25-man
 	else
-		return 90  -- 25-man: 90 seconds
+		return 110 -- 10-man
 	end
 end
 
 function mod:OnCombatStart(delay)
+	enrageTimer:Start(360 - delay)
 	local decimateTime = getDecimateTimer()
-	enrageTimer:Start(360 - delay) -- 6 minutes berserk timer
-	timerDecimate:Update(decimateTime, decimateTime) -- Set correct timer duration
 	timerDecimate:Start(decimateTime - delay)
-	warnDecimateSoon:Schedule(decimateTime - 10 - delay) -- 10 seconds before
+	warnDecimateSoon:Schedule(decimateTime - 10 - delay)
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 28371 or args.spellId == 54427 then
-		if self.Options.SpecWarn28371dispel then -- Updated option name
+		if self.Options.SpecWarn28371dispel then
 			specWarnEnrage:Show(args.destName)
 			specWarnEnrage:Play("enrage")
 		else
@@ -59,12 +55,11 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end
 
-function mod:SPELL_DAMAGE(_, _, _, _, _, _, spellId)
-	if (spellId == 28375 or spellId == 54426) and self:AntiSpam(20) then
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, spellName)
+	if (spellName == GetSpellInfo(28374) or spellName == GetSpellInfo(54426)) and self:AntiSpam(5, 1) then
 		local decimateTime = getDecimateTimer()
 		warnDecimateNow:Show()
-		timerDecimate:Update(decimateTime, decimateTime)
-		timerDecimate:Start()
+		timerDecimate:Start(decimateTime)
 		warnDecimateSoon:Schedule(decimateTime - 10)
 	end
 end
